a646eda826c283fa0b10d11ba8d787be8d08796a
Aristedes Maniatis <ari@ish.com.au>
2015-10-12 12:09:57 +1100

    Fix a problem exporting classes with more than one tag

--- export/classExtendedOutcomesCsvExport.groovy
+++ export/classExtendedOutcomesCsvExport.groovy
@@ -6,7 +6,7 @@ records.collectMany { cc -> cc.enrolments }.collectMany { e -> e.outcomes }.each
 			"lastName"              : o.enrolment.student.contact.lastName,
 			"classCode"             : o.enrolment.courseClass.uniqueCode,
 			"CourseName"            : o.enrolment.courseClass.course.name,
-			"Subject"               : o.enrolment.courseClass.course.tags?.collectMany { c -> c.name }?.join(" "),
+			"Subject"               : o.enrolment.courseClass.course.tags?.collect { t -> t.name }?.join(" "),
 			"QualNationalCode"      : o.enrolment.courseClass.course.qualification?.nationalCode,
 			"QualName"              : [o.enrolment.courseClass.course.qualification?.level, o.enrolment.courseClass.course.qualification?.title].findAll().join(" "),
 			"VETFlag"               : o.enrolment.courseClass.course.isVET,
